e66154763b6ba4697263be993b009a346bb83d85
WilsonValderrama <Wilson.valderrama10@gmail.com>
2022-07-06 17:45:58 -0500

    Primer commit

---
+++ mensajes_app/src/main/java/Conexion.java
@@ -0,0 +1,24 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion {
+
+    public Connection get_connection() {
+        Connection conection = null;
+        try {
+            conection = DriverManager.getConnection(
+                    "jdbc:mysql://localhost/mensajes_app",
+                    "root",
+                    ""
+            );
+            if (conection != null){
+                System.out.println("Conexion exitosa");
+            }
+        }catch (SQLException e){
+            System.out.println(e);
+        }
+        return conection;
+
+    }
+}
